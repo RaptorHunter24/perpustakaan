62d85c9128db2745f3974cd999fa2b6f0d172846
RaptorHunter24 <m.alif.jakarta@gmail.com>
2023-11-20 13:51:46 +0700

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,28 @@
+<p>
+    <!--awal FOOTER-->
+    <div class="col-md-12">
+        <div class="row" style="background:rgba(63, 67, 102, 0.562)">
+            <div class="col-md-9">
+                <h3>Visi</h3>
+                <p>Visi Polimedia yaitu menjadi lembaga pendidikan tinggi vokasi terkemuka dan unggul di bidang industri kreatif pada tahun 2030 di Asia Tenggara.
+                <p>
+                <h3>Misi</h3>
+                <ul>
+                    <li>Menyelenggarakan program pendidikan vokasi di bidang industri kreatif guna meningkatkan pengetahuan, keterampilan, dan kemampuan penerapannya; </li>
+                    <li>Membangun dan mengembangkan sumber daya manusia yang kreatif, inovatif, kompetitif, dan berakhlak mulia serta berwawasan kebangsaan;</li>
+                    <li>Membentuk program-program Pendidikan Vokasi yang relevan dengan bidang industri kreatif yang berbasis kompetensi produksi dan kompetensi kewirausahaan;</li>
+                    <li>Mengembangkan, menerapkan, dan menyebarluaskan teknologi di bidang industri kreatif yang berbasis pada kearifan lokal dan kebutuhan masyarakat serta dunia usaha dan industri; dan</li>
+                    <li>Menyelenggarakan dan mengembangkan tata kelola pendidikan yang efisien, akuntabel, transparan, dan berkeadilan.</li>
+                </ul>                     
+            </div>
+            <div class="col-md-3">
+                Copyright &copy; 2023. All right reserve
+                <p>
+                Developed by The Letter</div>
+        </div>
+    </div>
+    <!--akhir FOOTER-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+       
